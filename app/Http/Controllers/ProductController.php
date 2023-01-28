16d1198764c7a7cdb18a68786c213c121cbfa4fb
坂本碧 <c2131@oic.jp>
2023-01-28 14:16:33 +0900

    kadai3 save

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,17 +20,25 @@ class ProductController extends Controller
 
         Log::info($request->image_1 . 'です');
 
-        return response()->json(['returnFileName1' => 'ok']);
 
-        if ($file1 = $request->image_1) {
-            Log::debug($file1);
-            $fileName1 = time() . $file1->getClientOriginalName();
+        if ($request->has('image_1'))
+        {
+            $file_name = $request->file('image_1')->getClientOriginalName();
 
-            // $target_path1 = public_path('uploads/');
-            // ここでOSが違うことによるエラーがでる
-            $target_path1 = public_path('uploads\\');
-            $file1->move($target_path1, $fileName1);
-            return ['returnFileName1' => $fileName1];
+            $request->file('image_1')->storeAs('public', $file_name);
+
+            return response()->json(['returnFileName1' => $file_name]);
+
         }
+
+        // if ($file1 = $request->image_1) {
+        //     $fileName1 = time() . $file1->getClientOriginalName();
+
+        //     // $target_path1 = public_path('uploads/');
+        //     // ここでOSが違うことによるエラーがでる
+        //     $target_path1 = public_path('uploads\\');
+        //     $file1->move($target_path1, $fileName1);
+        //     return ['returnFileName1' => $fileName1];
+        // }
     }
 }
