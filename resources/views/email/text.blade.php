5b56d36bc7ae33413c45fc79380df61471b6dbc6
坂本碧 <c2131@oic.jp>
2023-01-28 13:53:52 +0900

    kadai3 sa

--- resources/views/email/text.blade.php
+++ resources/views/email/text.blade.php
@@ -8,8 +8,9 @@
 </div>
 <h2>パスワード再発行</h2>
 <div>以下のURLをクリックしてパスワードを再発行してください</div>
-@if (app()->envirionment(['local', 'staging']))
+@if (app()->environment(['local', 'staging']))
     <a href="http://localhost:8000/password-reset-page">http://localhost:8000/password-reset-page</a>
-@elseif (app()->envirionment(['production']))
+@elseif (app()->environment(['production']))
     <a href="https://ik1-407-35703.vs.sakura.ne.jp/password-reset-page">https://ik1-407-35703.vs.sakura.ne.jp/password-reset-page</a>
 @endif
+
