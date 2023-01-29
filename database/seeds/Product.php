6fa12ba836dfe24718934ef77cc646a3ec9a69d2
坂本碧 <c2131@oic.jp>
2023-01-30 00:44:33 +0900

    kadai4 finish

--- database/seeds/Product.php
+++ database/seeds/Product.php
@@ -11,17 +11,46 @@ class Product extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 3; $i++)
+        for($i = 1; $i <= 8; $i++)
         {
-            $random_int = random_int(16,20);
+            $random_int_for1 = random_int(1,5);
+            $random_int_for2 = random_int(6,10);
+            $random_int_for3 = random_int(11,15);
+            $random_int_for4 = random_int(16,20);
 
-            $a_int = $i + 28;
+            $a_int = $i + 1;
+            $b_int = $i + 10;
+            $c_int = $i + 20;
+            $d_int = $i + 30;
+            DB::table('products')->insert([
+                'member_id' => 1,
+                'product_category_id' => 1,
+                'product_subcategory_id' => $random_int_for1,
+                'name' => 'アイテム' . $a_int,
+                'product_content' => 'コンテンツ' . $a_int,
+            ]);
+
+            DB::table('products')->insert([
+                'member_id' => 1,
+                'product_category_id' => 2,
+                'product_subcategory_id' => $random_int_for2,
+                'name' => 'アイテム' . $b_int,
+                'product_content' => 'コンテンツ' . $b_int,
+            ]);
+
+            DB::table('products')->insert([
+                'member_id' => 1,
+                'product_category_id' => 3,
+                'product_subcategory_id' => $random_int_for3,
+                'name' => 'アイテム' . $c_int,
+                'product_content' => 'コンテンツ' . $c_int,
+            ]);
             DB::table('products')->insert([
                 'member_id' => 1,
                 'product_category_id' => 4,
-                'product_subcategory_id' => $random_int,
-                'name' => 'アイテム' . $a_int,
-                'product_content' => 'コンテンツ' . $a_int,
+                'product_subcategory_id' => $random_int_for4,
+                'name' => 'アイテム' . $d_int,
+                'product_content' => 'コンテンツ' . $d_int,
             ]);
         }
     }
