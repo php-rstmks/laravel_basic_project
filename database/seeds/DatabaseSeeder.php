ca9b523dbdbc5d7f1fc904800a21cbe53646332f
坂本碧 <c2131@oic.jp>
2023-01-30 00:48:46 +0900

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProductCategories::class);
-        $this->call(ProductSubcategories::class);
+        // $this->call(ProductCategories::class);
+        // $this->call(ProductSubcategories::class);
         $this->call(Product::class);
     }
 }
--- database/seeds/Product.php
+++ database/seeds/Product.php
@@ -11,7 +11,7 @@ class Product extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 8; $i++)
+        for($i = 1; $i <= 50; $i++)
         {
             $random_int_for1 = random_int(1,5);
             $random_int_for2 = random_int(6,10);
@@ -21,37 +21,37 @@ class Product extends Seeder
             $a_int = $i + 1;
             $b_int = $i + 10;
             $c_int = $i + 20;
-            $d_int = $i + 30;
+            $d_int = $i + 50;
             DB::table('products')->insert([
                 'member_id' => 1,
                 'product_category_id' => 1,
                 'product_subcategory_id' => $random_int_for1,
-                'name' => 'アイテム' . $a_int,
-                'product_content' => 'コンテンツ' . $a_int,
-            ]);
-
-            DB::table('products')->insert([
-                'member_id' => 1,
-                'product_category_id' => 2,
-                'product_subcategory_id' => $random_int_for2,
-                'name' => 'アイテム' . $b_int,
-                'product_content' => 'コンテンツ' . $b_int,
-            ]);
-
-            DB::table('products')->insert([
-                'member_id' => 1,
-                'product_category_id' => 3,
-                'product_subcategory_id' => $random_int_for3,
-                'name' => 'アイテム' . $c_int,
-                'product_content' => 'コンテンツ' . $c_int,
-            ]);
-            DB::table('products')->insert([
-                'member_id' => 1,
-                'product_category_id' => 4,
-                'product_subcategory_id' => $random_int_for4,
                 'name' => 'アイテム' . $d_int,
                 'product_content' => 'コンテンツ' . $d_int,
             ]);
+
+            // DB::table('products')->insert([
+            //     'member_id' => 1,
+            //     'product_category_id' => 2,
+            //     'product_subcategory_id' => $random_int_for2,
+            //     'name' => 'アイテム' . $b_int,
+            //     'product_content' => 'コンテンツ' . $b_int,
+            // ]);
+
+            // DB::table('products')->insert([
+            //     'member_id' => 1,
+            //     'product_category_id' => 3,
+            //     'product_subcategory_id' => $random_int_for3,
+            //     'name' => 'アイテム' . $c_int,
+            //     'product_content' => 'コンテンツ' . $c_int,
+            // ]);
+            // DB::table('products')->insert([
+            //     'member_id' => 1,
+            //     'product_category_id' => 4,
+            //     'product_subcategory_id' => $random_int_for4,
+            //     'name' => 'アイテム' . $d_int,
+            //     'product_content' => 'コンテンツ' . $d_int,
+            // ]);
         }
     }
 }
